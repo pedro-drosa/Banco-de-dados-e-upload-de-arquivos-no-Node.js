4c28d28a2a67df66ea785e2d1703c657e9ad791c
pedro-drosa <pedro_drosa@hotmail.com>
2021-02-27 13:52:48 -0300

    creating: migrations

---
+++ src/database/migrations/1614442651459-CreateCategories.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCategories1614442651459
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
